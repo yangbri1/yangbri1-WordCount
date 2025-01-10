16e6e2d69241d854023bafb80eca18df0aa995bf
Brian Yang <yangbri1@msu.edu>
2025-01-10 18:36:06 +0000

    Use for-each loop to iterate through given String 'in', count number of spaces, add to 'wordCount' which starts w/ value of 1 to account for the notion: each word is separated by a space (wordCount = space + 1)

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -5,6 +5,27 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        // if(in.contains(" "))
+        // call String.toCharArray() to convert given 'in' String into an array of characters
+        char[] charAry = in.toCharArray();
+        // initialize int 'wordCount' to count number of words detected in given String 'in'
+        // Note: In English structure each word is delimited by a space => reason for initialization to value 1
+        int wordCount = 1;
+        // for-each to iterate through each char in 'charAry'
+        for(char ch : charAry){
+            // check if current elem is of space
+            if(ch == ' '){
+                // prefix increment to count space
+                ++wordCount;
+            }
+        }
+        /* below while block also works */
+        // while(itr < in.length()){
+            // if(in.charAt(itr) == ' '){
+            //     wordCount++;
+            // }
+            // itr++;
+        // }
+        return wordCount;
     }
 }
